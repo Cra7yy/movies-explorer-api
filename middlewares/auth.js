a031ab14b2a013677f053f175d94dca9f226a5ad
Cra7y <cra7yy@gmail.com>
2022-09-16 18:50:51 +0300

    Critical bugs fixed

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const { checkToken } = require('../helpers/jwt');
+const User = require('../models/user');
+const AuthorizationError = require('../errors/authorizationError');
+
+const isAuthorized = (req, res, next) => {
+  const auth = req.headers.authorization;
+  if (!auth) {
+    next(new AuthorizationError('Авторизуруйтесь для доступа'));
+  }
+
+  const token = auth.replace('Bearer ', '');
+  try {
+    const payload = checkToken(token);
+    User.findOne({ email: payload.email })
+      .then((user) => {
+        if (!user) {
+          next(new AuthorizationError('Авторизуруйтесь для доступа'));
+        }
+        req.user = user._id;
+        next();
+      });
+  } catch (err) {
+    next(new AuthorizationError('Авторизуруйтесь для доступа'));
+  }
+};
+
+module.exports = {
+  isAuthorized,
+};
